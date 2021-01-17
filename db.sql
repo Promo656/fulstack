05bef9d8bf61d3d0ca3dd7edf01d7e91833cae5a
Egor <egormarkov1408@gmail.com>
2021-01-17 20:54:19 +0300

    delete db.sql

--- db.sql
+++
@@ -1,9 +0,0 @@
-create table reviews (
-id BIGSERIAL NOT NULL PRIMARY KEY,
-restaurant_id INT NOT NULL REFERENCES restaurants(id),
-name VARCHAR(50) NOT NULL,
-review  TEXT NOT NULL,
-rating INT NOT NULL check(rating >=1 and rating <=5)
-);
-
-
